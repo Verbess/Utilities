235079801ae077347948dcb0f307e8d105bcee6b
Verbess <verbess@foxmail.com>
2020-10-29 20:00:15 +0800

    feat(UnityEditor): add a script templates feature

---
+++ Unity/Editor/CreateTemplateScripts/RightClickMenu.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Verbess.Unity.Editor
+{
+    public partial class RightClickMenu : UnityEditor.Editor
+    {
+        private enum FileExtension
+        {
+            cs = 0,
+        }
+
+        /// <summary>
+        /// Get the directory path of selection.
+        /// </summary>
+        /// <returns>The directory path of selection.</returns>
+        private static string GetSelectionDirectoryPath()
+        {
+            string[] assetGUIDs = Selection.assetGUIDs;
+            string path = AssetDatabase.GUIDToAssetPath(assetGUIDs[0]);
+
+            if (path.Contains("."))
+            {
+                string[] splitPaths = path.Split('/');
+                path = "";
+                for (int i = 0; i < splitPaths.Length - 1; i++)
+                {
+                    path += splitPaths[i] + "/";
+                }
+            }
+            else
+            {
+                path += "/";
+            }
+
+            return path;
+        }
+
+        /// <summary>
+        /// Use to create template script.
+        /// </summary>
+        /// <param name="defaultFileName">The default name when file create.</param>
+        /// <param name="templateFileName">The file name of template.</param>
+        /// <param name="fileExtension">The extension type of file, it's a Enum.</param>
+        private static void CreateScriptTemplates(string defaultFileName, string templateFileName, FileExtension fileExtension)
+        {
+            string path = GetSelectionDirectoryPath();
+            string templateFilePath = Application.dataPath + $"/Editor/CreateTemplateScripts/ScriptTemplates/{templateFileName}";
+            string TargetFilePath = path + defaultFileName + $".{fileExtension.ToString()}";
+
+            bool IsCreated = false;
+            int index = 1;
+            while (!IsCreated)
+            {
+                if (!File.Exists(TargetFilePath))
+                {
+                    string content = File.ReadAllText(templateFilePath);
+                    ProjectWindowUtil.CreateAssetWithContent(TargetFilePath, content);
+                    IsCreated = true;
+                }
+                else
+                {
+                    TargetFilePath = path + defaultFileName + $"{index}.{fileExtension.ToString()}";
+                    index++;
+                }
+            }
+        }
+
+        [MenuItem("Assets/Create/Create Script Templates/C# Class - MonoBehaviour", false, 0)]
+        public static void CreateScriptTemplates_MonoBehaviourCSharp() => CreateScriptTemplates("NewBehaviourScript", "C# Script-NewBehaviourScript.cs.txt", FileExtension.cs);
+
+        [MenuItem("Assets/Create/Create Script Templates/C# Class", false, 100)]
+        public static void CreateScriptTemplates_CSharpClass() => CreateScriptTemplates("NewClassScript", "C# Script-NewClassScript.cs.txt", FileExtension.cs);
+
+        [MenuItem("Assets/Create/Create Script Templates/C# Interface", false, 100)]
+        public static void CreateScriptTemplates_CSharpInterface() => CreateScriptTemplates("NewInterfaceScript", "C# Script-NewInterfaceScript.cs.txt", FileExtension.cs);
+
+        [MenuItem("Assets/Create/Create Script Templates/C# Enum", false, 100)]
+        public static void CreateScriptTemplates_CSharpEnum() => CreateScriptTemplates("NewEnumScript", "C# Script-NewEnumScript.cs.txt", FileExtension.cs);
+    }
+}
---
+++ Unity/Editor/CreateTemplateScripts/ScriptModificationProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Verbess.Unity.Editor
+{
+    public class ScriptModificationProcessor : UnityEditor.AssetModificationProcessor
+    {
+        /// <summary>
+        /// Called when you create script asset.
+        /// </summary>
+        /// <param name="metaFilePath">script asset's meta file path.</param>
+        public static void OnWillCreateAsset(string metaFilePath)
+        {
+            string filePath = metaFilePath.EndsWith(".meta") ? metaFilePath.Replace(".meta", "") : metaFilePath;
+
+            // If file path has no file name extension, function return.
+            int fileNameExtensionindex = filePath.LastIndexOf(".");
+            if (fileNameExtensionindex < 0)
+            {
+                return;
+            }
+            // check file name extension, function return if it is not ".cs"
+            string fileNameExtension = filePath.Substring(fileNameExtensionindex + 1);
+            if (fileNameExtension != "cs")
+            {
+                return;
+            }
+
+            int fileIndex = filePath.LastIndexOf("/");
+            int index = Application.dataPath.LastIndexOf("Assets");
+            string globalFilePath = Application.dataPath.Substring(0, index) + filePath;
+            string file = File.ReadAllText(globalFilePath);
+            string _namespace = Application.companyName + "." + Application.productName;
+            string _fileName = filePath.Substring(fileIndex + 1, fileNameExtensionindex - fileIndex - 1);
+            file = file.Replace("#NAMESPACE#", _namespace);
+            file = file.Replace("#SCRIPTNAME#", _fileName);
+            File.WriteAllText(globalFilePath, file);
+            AssetDatabase.Refresh();
+        }
+    }
+}
